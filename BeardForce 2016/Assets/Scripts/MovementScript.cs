df99dacf96ebaaa45246ad84ad3c89b5b59fd0b7
Simon Lysell <lysell.simon94@gmail.com>
2016-04-14 11:51:17 +0200

    Added a walking script for the player
    Still need to solve issue with player either falling through the floor
    or tip over something

---
+++ BeardForce 2016/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+
+    // Variables
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float gravity;
+
+    private float moveX;
+    private float moveY;
+	
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody>().velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+
+
+
+	}
+}
