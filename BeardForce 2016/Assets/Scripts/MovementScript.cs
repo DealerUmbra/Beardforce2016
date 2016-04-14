e89f188efe11c82ea198f672b9b280f85d5199eb
Simon Lysell <lysell.simon94@gmail.com>
2016-04-14 21:39:30 +0200

    Did some changes to the movement script

--- BeardForce 2016/Assets/Scripts/MovementScript.cs
+++ BeardForce 2016/Assets/Scripts/MovementScript.cs
@@ -11,17 +11,26 @@ public class MovementScript : MonoBehaviour
 
     private float moveX;
     private float moveY;
-	
 
-	// Update is called once per frame
-	void FixedUpdate ()
+    private Vector2 move;
+    //private Vector2 gravityDown;
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
     {
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
 
-        GetComponent<Rigidbody>().velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+        // Old code using the RigidBody for movement
+        // GetComponent<Rigidbody>().velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+
+        // Movement code using transform
+        move = new Vector3(moveX, moveY, 0);
+        transform.position = move * moveSpeed * Time.deltaTime;
 
 
-
-	}
+        //Code for gravity
+        /*gravityDown = new Vector2(0, gravity);
+        transform.position = gravityDown * gravity * Time.deltaTime;*/
+    }
 }
