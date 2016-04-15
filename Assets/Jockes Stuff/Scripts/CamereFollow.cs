4ee33e91776aea4cbc2c379858ef824a4e58813b
jalajaala <joacim_hedin@hotmail.com>
2016-04-15 12:16:55 +0200

    Added Stuff Till Jockes Stuff

---
+++ Assets/Jockes Stuff/Scripts/CamereFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamereFollow : MonoBehaviour {
+    private Transform target;
+    public float CameraY;
+
+
+	// Use this for initialization
+	void Start () {
+        
+        target = GameObject.FindGameObjectWithTag("PlayerDude").transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = new Vector3(target.position.x, target.position.y + CameraY, gameObject.transform.position.z);
+    }
+}
