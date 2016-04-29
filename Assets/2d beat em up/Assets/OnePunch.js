dc5543c9005c8804abbf1dd823042a1dc2e38322
Bro-man <filip19952009@gmail.com>
2016-04-29 10:33:39 +0200

    Movement, Punch, enemy move towards you and stop right before
    Movement, Punch, enemy move towards you and stop right before, enemy
    dies when health = 0

---
+++ Assets/2d beat em up/Assets/OnePunch.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+ var Punch = false;
+ function Update() 
+ {
+     if (Input.GetKeyDown(KeyCode.E)) 
+     {
+         transform.Translate(0.7, 0, 0, Camera.main.transform);
+         Punch = true;
+     }
+     if (Input.GetKeyUp(KeyCode.E)) 
+     {
+         transform.Translate(-0.7, 0, 0, Camera.main.transform);
+         Punch = true;
+     }
+ }
+ 
+ 
