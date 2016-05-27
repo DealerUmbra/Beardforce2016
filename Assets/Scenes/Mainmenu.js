3ab52fde00fe31edb8498c8f165d11af7cd08303
Sajberwolf <philip.hedqvist@hotmail.com>
2016-05-27 15:57:43 +0200

    kdald

---
+++ Assets/Scenes/Mainmenu.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+function OnGUI () 
+{
+    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+    if(GUI.Button (Rect (Screen.width/2 - 100, Screen.height/2, 160, 50), "Play")) 
+    {
+        Application.LoadLevel ("All_Levels");
+    }
+}
